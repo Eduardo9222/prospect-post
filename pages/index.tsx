3813be8f28d166cc4466f71466d6af2ebd175b56
Eduardo9222 <evc9222@gmail.com>
2025-06-06 05:03:25 +0800

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
 import About from "@/components/About";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <Hero />
       <About />
     </div>
   );
